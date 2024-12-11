a4b0430c0bbc85454d5980655f2da8629d81be75
sonyapaleolog <semenovasofyaeduardovna@gmail.com>
2024-12-11 14:45:40 +0300

    lint was added to package.json

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,13 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {files: ["js/**/*.{js,mjs,cjs,jsx}"]},
   {ignores: [".node_modules/*"]},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
 ];
