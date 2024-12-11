9f48b43bddd5a13e9657eb281d2d25cf10de1d9d
sonyapaleolog <semenovasofyaeduardovna@gmail.com>
2024-12-11 09:53:33 +0300

    attempt to remove node_modules from consideration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,6 +7,7 @@ import pluginReact from "eslint-plugin-react";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {ignores: [".node_modules/*"]},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
