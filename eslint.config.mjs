aa359e97e145b280cbfa0bf0e07409e1e0e88ba1
sonyapaleolog <semenovasofyaeduardovna@gmail.com>
2024-12-28 23:49:16 +0300

    new one

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,5 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
 
 
 /** @type {import('eslint').Linter.Config[]} */
@@ -9,5 +8,4 @@ export default [
   {ignores: ["node_modules/*"]},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
 ];
